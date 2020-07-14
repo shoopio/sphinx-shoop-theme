e0fdc47a1a16a89747865b735e9e017da4651b96
William <wilindho@gmail.com>
2020-07-14 17:43:53 +0300

    Update theme for easy development
    - Update sphinx so it's up to the newest version
    - Update the hole theme to use the npm webpack package for development of the theme.
    The webpack package is what the latest sphinx themes uses.
    Running the dev server works fine read in the readme file how to set up the dev server.
    Testing the theme changes should be done in first hand on the local dev server but if you ant to test it with the shuup guide there is documentation for that to.
    - Change nav bar color to be black with white text

--- README.rst
+++ README.rst
@@ -1,7 +1,36 @@
-Sphinx Theme for Shoop Documentation
+Sphinx Theme for Shuup Documentation
 ====================================
 
-This is a Sphinx theme for Shoop's documentation.
+This is a Sphinx theme for Shuup's documentation.
 
 The theme is a fork of `Read the Docs Sphinx Theme
 <https://github.com/snide/sphinx_rtd_theme>`.
+
+
+Development
+===========
+
+1. Install requirements `pip install -r requirements.txt`
+2. Install npm packages `npm i`
+3. Run the dev server `npm run dev`
+4. Refresh the page after the browser tab opens up and it says "Cannot GET /"
+
+This will launch the dev server on port 2000.
+All changes made to the theme will be automatically get refreshed on the dev server.
+
+
+Development with shuup-guide
+============================
+
+To se changes made to the theme in the shuup-guide you will need to do the following:
+
+1. Install requirements `pip install -r requirements.txt`
+2. Install npm packages `npm i`
+3. Build the static files `npm run build`
+4. Follow the steps in the shuup-guide readme
+
+
+Updating theme/pushing new updates
+===================================
+
+To update the theme to contain latest change just run `python setup.py build` and then commit your changes.
--- setup.py
+++ setup.py
@@ -1,51 +1,105 @@
+# -*- coding: utf-8 -*-
+"""`sphinx_shuup_theme` lives on `Github`_.
+
+.. _github: https://github.com/rtfd/sphinx_shuup_theme
+
+"""
+
 import os
-
-import setuptools
-
-
-def _get_version():
-    filepath = os.path.join('sphinx_shoop_theme', '__init__.py')
-    with open(filepath) as fp:
-        for line in fp:
-            if line.startswith("__version__ = '"):
-                return line.strip().split("'", 2)[1]
-    raise EnvironmentError("Cannot detect version from %s" % filepath)
+import subprocess
+import distutils.cmd
+import setuptools.command.build_py
+from io import open
+from setuptools import setup
 
 
-def _get_long_description():
-    with open('README.rst') as fp:
-        return fp.read()
+class WebpackBuildCommand(setuptools.command.build_py.build_py):
+
+    """Prefix Python build with Webpack asset build"""
+
+    def run(self):
+        if not 'CI' in os.environ and not 'TOX_ENV_NAME' in os.environ:
+            subprocess.run(['npm', 'install'], check=True)
+            subprocess.run(['node_modules/.bin/webpack', '--config', 'webpack.prod.js'], check=True)
+        setuptools.command.build_py.build_py.run(self)
 
 
-if __name__ == '__main__':
-    setuptools.setup(
-        name='sphinx_shoop_theme',
-        version=_get_version(),
-        url='https://github.com/shoopio/sphinx_shoop_theme/',
-        license='MIT',
-        author='Shoop Ltd (original by Dave Snider)',
-        description='Sphinx Theme for Shoop',
-        long_description=_get_long_description(),
-        zip_safe=False,
-        packages=['sphinx_shoop_theme'],
-        package_data={'sphinx_shoop_theme': [
-            'theme.conf',
-            '*.html',
-            'static/css/*.css',
-            'static/js/*.js',
-            'static/font/*.*'
-        ]},
-        include_package_data=True,
-        classifiers=[
-            'Development Status :: 3 - Alpha',
-            'License :: OSI Approved :: BSD License',
-            'Environment :: Console',
-            'Environment :: Web Environment',
-            'Intended Audience :: Developers',
-            'Programming Language :: Python :: 2.7',
-            'Programming Language :: Python :: 3',
-            'Operating System :: OS Independent',
-            'Topic :: Documentation',
-            'Topic :: Software Development :: Documentation',
+class WebpackDevelopCommand(distutils.cmd.Command):
+
+    description = "Run Webpack dev server"
+
+    user_options = []
+
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        pass
+
+    def run(self):
+        subprocess.run(
+            ["node_modules/.bin/webpack-dev-server", "--open", "--config", "webpack.dev.js"],
+            check=True
+        )
+
+
+setup(
+    name='sphinx_shuup_theme',
+    version='0.6.0',
+    url='https://github.com/rtfd/sphinx_shuup_theme/',
+    license='MIT',
+    author='Shoop Ltd (original by Dave Snider)',
+    description='Sphinx Theme for Shoop',
+    long_description=open('README.rst', encoding='utf-8').read(),
+    cmdclass={
+        'build_py': WebpackBuildCommand,
+        'watch': WebpackDevelopCommand,
+    },
+    zip_safe=False,
+    packages=['sphinx_shuup_theme'],
+    package_data={'sphinx_shuup_theme': [
+        'theme.conf',
+        '*.html',
+        'static/css/*.css',
+        'static/css/fonts/*.*'
+        'static/js/*.js',
+    ]},
+    include_package_data=True,
+    # See http://www.sphinx-doc.org/en/stable/theming.html#distribute-your-theme-as-a-python-package
+    entry_points = {
+        'sphinx.html_themes': [
+            'sphinx_shuup_theme = sphinx_shuup_theme',
+        ]
+    },
+    install_requires=[
+        'sphinx'
+    ],
+    tests_require=[
+        'pytest',
+    ],
+    extras_require={
+        'dev': [
+            'transifex-client',
+            'sphinxcontrib-httpdomain',
+            'bump2version',
         ],
-    )
+    },
+    classifiers=[
+        'Framework :: Sphinx',
+        'Framework :: Sphinx :: Theme',
+        'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: BSD License',
+        'Environment :: Console',
+        'Environment :: Web Environment',
+        'Intended Audience :: Customers',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Operating System :: OS Independent',
+        'Topic :: Documentation',
+        'Topic :: Software Development :: Documentation',
+    ],
+)
